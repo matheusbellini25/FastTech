5c64cedff26416f842b99120e1b8bd31fd92f33b
Gabriel Loureiro <gabrielloureirog@gmail.com>
2025-03-25 20:21:20 -0300

    :hammer: Ajustes nas configurações das métricas

--- src/MPCalcHub.Api/Program.cs
+++ src/MPCalcHub.Api/Program.cs
@@ -47,6 +47,7 @@ if (string.IsNullOrEmpty(jwtKeyConfig))
     throw new InvalidOperationException("Token:Key configuration is missing or empty.");
 
 builder.Services.Configure<MPCalcHubSettings>(builder.Configuration);
+builder.Services.AddHealthChecks().ForwardToPrometheus();
 builder.WebHost.UseUrls("https://0.0.0.0:5056");
 
 builder.Services.AddAuthentication(o =>
@@ -213,6 +214,8 @@ using (var scope = app.Services.CreateScope())
     context.Database.Migrate();
 }
 
+
+app.UseHealthChecks("/health");
 app.UseHttpMetrics();
 app.MapMetrics();
 

09fd1e08c772a65e213d6f8b67664cc9bff065fe
Julio Cezar Bueno Ribeiro <amcom-julio.ribeiro@ailos.coop.br>
2025-07-19 23:01:37 -0300

    commit de atualizacao

--- FastTechCore/src/FastTech.Infrastructure/obj/Debug/net8.0/FastTech.Infrastructure.AssemblyInfo.cs
+++ FastTechCore/src/FastTech.Infrastructure/obj/Debug/net8.0/FastTech.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FastTech.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f334051722b2068d5ea312f911dd07e26ab544a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54208cb3f3dac7383d20291ad93a7c9f54ec95a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("FastTech.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FastTech.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- tests/FastTech.Tests/obj/Debug/net8.0/FastTech.Tests.AssemblyInfo.cs
+++ tests/FastTech.Tests/obj/Debug/net8.0/FastTech.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FastTech.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f334051722b2068d5ea312f911dd07e26ab544a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54208cb3f3dac7383d20291ad93a7c9f54ec95a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("FastTech.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FastTech.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
